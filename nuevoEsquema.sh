6b98fdd6da8dcd3aef21409b4040687c170b0493
leandrodrc21 <51094309+leandrodrc21@users.noreply.github.com>
2019-10-15 13:49:00 -0500

    Update nuevoEsquema.sh

--- nuevoEsquema.sh
+++ nuevoEsquema.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
-PASSWORD="${2}"
+PASSWORD=$1
 mysql -u root -p << EOF
 CREATE USER 'admin'@'localhost' IDENTIFIED BY '134679';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 CREATE USER 'admin'@'%' IDENTIFIED BY '134679';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+echo "usuario creado";
 DROP USER 'root'@'localhost';
+echo "usuario root eliminado";
 create database IF NOT EXISTS  miphp;
 CREATE USER IF NOT EXISTS  'admin'@'localhost' identified by PASSWORD;
 GRANT ALL PRIVILEGES ON miphp.* TO php@localhost;
