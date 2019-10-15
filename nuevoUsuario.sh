3ae24e2b78b2db0c475bb78598364b9ce87fe9d5
leandrodrc21 <51094309+leandrodrc21@users.noreply.github.com>
2019-10-15 16:08:47 -0500

    Update nuevoUsuario.sh

--- nuevoUsuario.sh
+++ nuevoUsuario.sh
@@ -7,5 +7,11 @@ GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 CREATE USER 'admin'@'%' IDENTIFIED BY '134679';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+create database IF NOT EXISTS  miphp;
+CREATE USER IF NOT EXISTS  'php'@'localhost' identified by $1;
+GRANT ALL PRIVILEGES ON miphp.* TO php@localhost;
+FLUSH PRIVILEGES;
+use miphp;
+CREATE TABLE  IF NOT EXISTS clientes (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(30), apellido VARCHAR(30), suscripcion_activa CHAR(1), vencimiento_contrato DATE);
 DROP USER 'root'@'localhost';
 EOF
